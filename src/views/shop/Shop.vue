97449d37393b9ba796da87298f7bf7c43085201e
v3-vite-test <876023825@qq.com>
2024-06-14 18:00:29 +0800

    提交代码

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -9,7 +9,7 @@
         <router-link to="/shop/ratings">评价</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/shop/info">商家</router-link>
+        <router-link to="/shop/info">商家1</router-link>
       </div>
     </div>
     <keep-alive>
